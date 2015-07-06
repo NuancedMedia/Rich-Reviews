08412589c2f0059760fb71c9c42d269e137f8b5c
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-07-06 10:36:58 -0700

    removing old code from shortcodes/rr-snippet.php

--- shortcodes/rr-snippet.php
+++ shortcodes/rr-snippet.php
@@ -14,7 +14,7 @@
 			}
 			$stars = '';
 			$star_count = 0;
-			//dump($averageRating, 'AVE:');
+
 			for ($i=1; $i<=5; $i++) {
 				if ($i <= $roundedAverage) {
 					$stars = $stars . '&#9733;';
@@ -34,30 +34,5 @@
 		);
 
 		include '/../views/frontend/snippets.php';
-
-			// ----- Old Star handling that broke Google Snippets because
-			// ----- $average rating was being altered.
-
-			// while($averageRating >= 1) {
-			// 	$stars = $stars . '&#9733';
-			// 	$star_count++;
-			// 	$averageRating--;
-			// 	//dump($averageRating, 'AVE in WHILE:');
-			// 	//dump($star_count, 'STAR COUNT:');
-			// }
-			// while ($star_count < 5) {
-			// 	$stars = $stars . '&#9734';
-			// 	$star_count++;
-			// 	//dump($star_count, 'STAR COUNT:');
-			// }
-
-		// 	$output = '<div class="hreview-aggregate">Overall rating: <span class="stars">' . $stars . '</span> <span class="rating" style="display: none !important;">' . $averageRating . '</span> based on <span class="votes">' . $approvedReviewsCount . '</span> reviews</div>';
-		// 	$this->render_custom_styles();
-		// } else {
-		// 	$output = '<div class="hreview-aggregate">Overall rating: <span class="rating">' . $averageRating . '</span> out of 5 based on <span class="votes">' . $approvedReviewsCount . '</span> reviews</div>';
-		// }
-
-
-
 
 	}
