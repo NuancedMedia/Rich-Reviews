10efb755bf46f46fa4f50ae798181ec4ffbeeb6c
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-07-06 10:38:14 -0700

    removing deprecated code from shortcodes/rr-show.php

--- shortcodes/rr-show.php
+++ shortcodes/rr-show.php
@@ -49,8 +49,6 @@
 
 		}
 		do_action('rr_close_testimonial_group', $options);
-		// render_custom_styles($options);
-		// return __($output, 'rich-reviews');
 	}
 
 	function display_review($review, $options) {
@@ -98,81 +96,6 @@
 		do_action('rr_do_review_wrapper', $data);
 
 		do_action('rr_do_review_content', $data);
-
-		// $output = '<div class="testimonial">
-		// 	<h3 class="rr_title">' . $rTitle . '</h3>
-		// 	<div class="clear"></div>';
-		// if ($options['show_form_post_title']) {
-		// 	$output .= '<div class="rr_review_post_id"><a href="' . get_the_permalink($rPostId) . '">' . get_the_title($rPostId) . '</a></div><div class="clear"></div>';
-		// }
-		// $output .= '<div class="stars">' . $rRating . '</div>
-		// 	<div class="clear"></div>';
-		// $output .= '<div class="rr_review_text"><span class="drop_cap">“</span>' . $rText . '”</div>';
-		// $output .= '<div class="rr_review_name"> - ' . $rName . '</div>
-		// 	<div class="clear"></div>';
-		// $output .= '</div>';
-
-		// #TODO: Rework output for rich data, image, and up/down vote
-		// if($options['display_full_width'] != NULL) {
-		// 	$output = '<div class="full-testimonial" itemscope itemtype="http://schema.org/Review">';
-		// 	$output .= '<div class="review-head">';
-		// 	// if($rAuthorImage) {
-		// 	// 	dump($rAuthorImage);
-		// 	// 	$output .= '<div class="user-image">';
-		// 	// 	$output .= wp_get_attachment_image( $rAuthorImage, [70, 70]);
-		// 	// 	$output .= '</div>';
-		// 	// }
-		// 	$output .= '<div class="review-info">';
-		// 	if( $rTitle != '') {
-		// 		$output .= '<h3 class="rr_title">' . $rTitle . '</h3>';
-		// 	} else {
-		// 		$output .= '<h3 class="rr_title" style="display:none">' . $rTitle . '</h3>';
-		// 	}
-		// 	$output .= '<div class="clear"></div>';
-		// } else {
-		// 	$output = '<div class="testimonial" itemscope itemtype="http://schema.org/Review">';
-		// 	if( $rTitle != '') {
-		// 		$output .= '<h3 class="rr_title" itemprop="name">' . $rTitle . '</h3>';
-		// 	} else {
-		// 		$output .= '<h3 class="rr_title" style="display:none">'.$rTitle . '</h3>';
-		// 	}
-		// 	$output .= '<div class="clear"></div>';
-		// }
-		// if ($options['show_form_post_title']) {
-		// 	$output .= '<span itemprop="itemReviewed" itemscope itemtype="http://schema.org/Product"><div class="rr_review_post_id" itemprop="name"><a href="' . get_permalink($rPostId) . '">' . get_the_title($rPostId) . '</a></div><div class="clear"></div></span>';
-		// } else {
-		// 	$output .= '<span itemprop="itemReviewed" itemscope itemtype="http://schema.org/Product"><div class="rr_review_post_id" itemprop="itemreviewed" style="display:none;"><a href="' . get_permalink($rPostId) . '">' . get_the_title($rPostId) . '</a></div><div class="clear"></div></span>';
-		// }
-		// #TODO: Double check to ensure date formatting is correct
-		// if ($options['show_date']) {
-		// 	if($rDateTime != "0000-00-00 00:00:00") {
-		// 		$output .= '<span class="rr_date"><meta itemprop="datePublished" content="'.$rDateTime.'"><time datetime="' . $rDate . '">' . $rDate . '</time></span>';
-		// 	} else {
-		// 		if(current_user_can('edit_posts')) {
-		// 		$output .= '<span class="date-err rr_date">' . __('Date improperly formatted, correct in ', 'rich-reviews') . '<a href="/wp-admin/admin.php?page=fp_admin_approved_reviews_page">' . __('Dashboard', 'rich-reviews') . '</a></span>';
-		// 		}
-		// 	}
-		// } else {
-		// 	if($rDateTime != "0000-00-00 00:00:00") {
-		// 		$output .= '<span class="rr_date" style="display:none"><meta itemprop="datePublished" content="'.$rDateTime.'"><time datetime="' . $rDate . '">' . $rDate . '</time></span>';
-		// 	}
-		// }
-		// $output .= '<div class="stars">' . $rRating . '</div><div style="display:none;" itemprop="reviewRating" itemscope itemtype="http://schema.org/Rating"><span itemprop="ratingValue">' . $rRatingVal . '</span><span itemprop="bestRating">5</span><span itemprop="worstRating">1</span></div>';
-
-		// $output .= '<div class="clear"></div>';
-		// if($options['display_full_width']) {
-		// 	$output .= '</div></div>';
-		// }
-		// if($rText != '') {
-		// 	$output .= '<div class="rr_review_text"  ><span class="drop_cap">“</span><span itemprop="reviewBody">' . $rText . '</span>”</div>';
-		// }
-		// if( $rName !='' ) {
-		// 	$output .= '<div class="rr_review_name" itemprop="author" itemscope itemtype="http://schema.org/Person"> - <span itemprop="name">' . $rName . '</span></div>';
-		// }
-		// $output .=	'<div class="clear"></div>';
-		// $output .= '</div>';
-		// return __($output, 'rich-reviews');
-
 
 	}
 
