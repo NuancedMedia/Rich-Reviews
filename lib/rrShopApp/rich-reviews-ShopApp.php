3d11eb814bf9b536f20b98be8388ef1b613a2a59
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-10-21 10:25:22 -0700

    moving shopper approved files to own folder

---
+++ lib/rrShopApp/rich-reviews-ShopApp.php
@@ -0,0 +1,373 @@
+<?php
+
+require_once('rich-reviews-ShopAppOptions.php');
+wp_cron();
+
+
+class RRShopApp {
+
+	var $admin;
+
+	var $option_name;
+
+	var $options;
+
+
+	function __construct($parent = null) {
+
+		$this->parent = $parent;
+		$this->option_name = 'shopAppOptions';
+		$this->options = new RRShopAppOptions($this);
+		$this->shopAppOptions = $this->options->get_option();
+
+		add_shortcode('shopper-approved', array(&$this, 'display_handle'));
+		add_action('admin_menu', array(&$this, 'add_admin_page'));
+		add_action('init', array(&$this, 'init'));
+		 add_shortcode('run_pull', array($this, 'process_reviews_pull'));
+		date_default_timezone_set('MST');
+
+
+	}
+
+	function init() {
+		$this->options->update_options();
+		add_action('update_cache', array(&$this, 'cron_update_cache'));
+
+		if ( ! wp_next_scheduled( 'update_cache' ) ) {
+		  wp_schedule_event( time(), 'daily', 'update_cache' );
+		}
+	}
+
+	function cron_update_cache() {
+		$this->process_cron_update();
+	}
+
+	function display_handle($atts) {
+		$stuff = $this->options->get_option();
+		if(isset($stuff['markup']) && $stuff['markup'] != '') {
+			$html = $stuff['markup'];
+			return $html;
+		}
+			return;
+	}
+
+	function add_admin_page() {
+		add_menu_page( 'Shop App Cache', 'Shop App Cache', 'manage_options', 'shop_app_aid_menu', array(&$this, 'display_shop_app_aid_menu'));
+	}
+
+	function process_cache_update($data) {
+
+		if($data == null) {
+			$data = $this->shopAppOptions;
+		}
+		// extract($data);
+		//make sure the api_url is actually pointing at a file file before trying to retreive content. If not everything will simply remain unchanged.
+		$pattern = '/https:\/\/www\.shopperapproved\.com\//';
+        $accurate_call = preg_match($pattern, $data['api_url']);
+        if(!$accurate_call) {
+        	//bad api call
+        	return;
+        }
+
+        $data = $this->update_site_keys($data);
+
+		//dump(wp_remote_fopen($api_url));
+
+
+		$this->update_reviews_general_info($data);
+
+        $data = $this->update_aggregate_snippet_markup($data);
+
+		// if(isset($markup) && $markup != null && $markup != '') {
+		$data['last_update'] = date("F j, Y, g:i a");
+
+			// $return = array(
+			// 	'api_url' 		=> 	$api_url,
+			// 	'markup'		=>	$markup,
+			// 	'last_update'	=>	$last_update
+			// );
+
+		return $data;
+		// } else {
+		// 	// did not properly return markup
+		// 	return;
+		// }
+	}
+
+	public function update_site_keys($data) {
+
+        if(!isset($data['api_url']) || $data['api_url'] == '') {
+          return $data;
+        } else {
+          $url = $data['api_url'];
+        }
+
+        $urlParts = explode('?', $url);
+
+        if(!is_array($urlParts) || !isset($urlParts[1]) || count($urlParts) !=  2) {
+          return $data;
+        }
+        $urlParamString = $urlParts[1];
+        parse_str($urlParamString);
+        dump($token);
+        if(isset($siteid) && isset($token)) {
+            $data['site_id'] = $siteid;
+            $data['site_token'] = $token;
+        }
+
+        return $data;
+    }
+
+    public function update_aggregate_snippet_markup($data) {
+
+    	if(!isset($data['site_id']) || $data['site_id'] == '' || !isset($data['site_token']) || $data['site_token'] == '' ) {
+            return $data;
+        }
+
+        $json_request = "https://www.shopperapproved.com/feeds/schema.php?siteid=" . $data['site_id'] . "&token=" . $data['site_token'];
+
+
+    	if(wp_remote_fopen($json_request) != false) {
+			$markup = file_get_contents($json_request);
+			$data['markup'] = $markup;
+		} else {
+			// No file at url
+			return $data;
+		}
+
+		return $data;
+    }
+
+    public function update_reviews_info() {
+      $data = $this->options->get_option();
+      $this->update_reviews_general_info($data);
+    }
+
+    public function update_reviews_general_info($data) {
+        if(!isset($data['site_id']) || $data['site_id'] == '' || !isset($data['site_token']) || $data['site_token'] == '' ) {
+            return $data;
+        }
+
+        $json_request = "https://www.shopperapproved.com/api/sites/?siteid=" . $data['site_id'] . "&token=" . $data['site_token'];
+        if(wp_remote_fopen($json_request) != false) {
+          $response = file_get_contents($json_request);
+        } else {
+          // No file at url
+          return $data;
+        }
+        $response = json_decode($response);
+
+        if(isset($response->average)) {
+          $this->options->update_option(array('average_score' => $response->average));
+        }
+
+        if(isset($response->review_count)) {
+          $this->options->update_option(array('total_review_count' => $response->review_count));
+        }
+
+        dump($this->options->get_option());
+    }
+
+    public function process_reviews_pull() {
+
+		$data = $this->shopAppOptions;
+ 		if(!isset($data['site_id']) || $data['site_id'] == '' || !isset($data['site_token']) || $data['site_token'] == '' ) {
+            return $data;
+        }
+
+        $current_pulled_reviews = $data['reviews_pulled_count'];
+        $total_shop_app_reviews = $data['total_review_count'];
+
+        //Need to figure out how to sync effectively, probably using date params or page. Issue is only 100 reviews are pulled at a time.
+
+        $reviews_json = $this->fetch_shopper_approved_reviews($data);
+        $reviews_array = json_decode($reviews_json);
+
+        $inserted_ids = $data['imported_review_ids'];
+        foreach($reviews_array as $id => $review_object) {
+        	if(!in_array($id, $inserted_ids)) {
+        		$this->insert_shop_app_review($review_object, $id);
+        	}
+        }
+
+        dump($reviews_array);
+
+    }
+
+    public function insert_shop_app_review($review, $id) {
+
+    	$options = $this->shopAppOptions;
+
+    	if(in_array($id, $options['imported_review_ids'])) {
+    		return;
+    	}
+
+    	dump($id);
+    	dump($review);
+
+  //   	$newSubmission = array(
+		// 	'date_time'       => $newData['date_time'],
+		// 	'reviewer_name'   => $newData['reviewer_name'],
+		// 	// 'reviewer_image_id' => $newData['reviewer_image_id'],
+		// 	'reviewer_email'  => $newData['reviewer_email'],
+		// 	'review_title'    => $newData['review_title'],
+		// 	'review_rating'   => intval($newData['review_rating']),
+		// 	// 'review_image_id' => $newData['review_image_id'],
+		// 	'review_text'     => $newData['review_text'],
+		// 	'review_status'   => $newData['review_status'],
+		// 	'reviewer_ip'     => $newData['reviewer_ip'],
+		// 	'post_id'		  => $newData['post_id'],
+		// 	'review_category' => $newData['review_category'],
+		// );
+
+    }
+
+    public function fetch_shopper_approved_reviews($data = null) {
+
+    	if(!isset($data['site_id']) || $data['site_id'] == '' || !isset($data['site_token']) || $data['site_token'] == '' ) {
+            return $data;
+        }
+
+        $json_request = 'https://www.shopperapproved.com/api/reviews/?siteid=' . $data['site_id'] . '&token=' . $data['site_token'] . '';
+
+        //maybe append remaining parameters, based on options or synching strategy
+
+        if(wp_remote_fopen($json_request) != false) {
+			$reviews_json = file_get_contents($json_request);
+			return $reviews_json;
+		} else {
+			// No file at url
+			return null;
+		}
+
+
+    }
+
+	// function pull_new_SA_reviews() {
+	// 	$query =
+	// }
+
+	// Nice try but get_headers throws errors if domain doesn't exist
+
+	// function url_exists($url) {
+	// 	$headers = get_headers($url);
+	// 	if(strpos($headers[0], "200 OK")) {
+	// 		return true;
+	// 	}
+	// 	return false;
+	// }
+
+	// helper for file_get_contents() pre-call check
+	// function is_url_exist($url){
+	//     $ch = curl_init($url);
+	//     curl_setopt($ch, CURLOPT_NOBODY, true);
+	//     curl_exec($ch);
+	//     $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+	//     if($code == 200){
+	//        $status = true;
+	//     }else{
+	//       $status = false;
+	//     }
+	//     curl_close($ch);
+	//    return $status;
+	// }
+
+	function process_cron_update() {
+
+	    $updated_data = $this->process_cache_update();
+	    $data = array();
+	    foreach ($updated_data as $key=>$value) {
+	        if ($key != 'dinner' && $key != NULL) {
+	            $data[$key] = $value;
+	        }
+	    }
+	    $this->options->update_option($data);
+	    $this->shopAppOptions = $this->options->get_option();
+  	}
+
+	function display_shop_app_aid_menu() {
+		$this->options->update_options();
+		?>
+		<div class="rr_shortcode_container">
+		<h1>Shopper Approved Cache Menu</h1>
+		<br />
+		<!-- check if this is already set -->
+		<form id="shopAppAdmin"method="post" action="">
+			<input type="hidden" name="dinner" value="served" />
+			<div class="label-container one-fifth" style="width:30%;float:left;">
+				<label for="api_url" style="float:right;font-size:18px;">
+					Enter API Url:
+				</label>
+			</div>
+			<div class="input-container two-thirds" style="width:66%;float:right;">
+				<input type="text" name="api_url" style="width: 70%;float:left;" placeholder="API URL" style="vertical-align:bottom;" <?php if($this->shopAppOptions['api_url'] != null) { echo 'value="' . $this->shopAppOptions['api_url'] . '"';} ?> />
+			</div>
+			<div class="clear"></div>
+			<br />
+			<div class="input-container two-thirds" style="width:66%;float:right;">
+			<div style="width: 70%;float:left;">
+				<input type="submit" class="button" id="submit-api-url" value="Submit Url" style="float:right;" />
+			</div>
+		</div>
+		</form>
+		<div class="clear"></div>
+		<br />
+		<div class="label-container one-fifth" style="width:30%; float:left;">
+			<label for="html-markup" style="float:right;font-size:18px;">
+				Shopper Approved Markup:
+				<div style="font-size: 11px; font-weight: 400; margin-top: 8px; font-style: italic;">Use the shortcode <code style="font-style: normal; font-size: 11px;">[shopper-approved]</code> to output this markup</div>
+			</label>
+		</div>
+		<div class="input-container two-thirds" style="width:66%;float:right;">
+			<code name="Shopper Approved" placeholder="API Key" rows="10" cols="70" style="overflow:scroll;width:70%;float:left;" >
+			<?php if($this->shopAppOptions['markup'] != null) { echo htmlspecialchars($this->shopAppOptions['markup']);} ?>
+			</code>
+		</div>
+		<div class="clear"></div>
+		<br />
+		<div class="label-container one-fifth" style="width:30%;float:left;">
+			<label for="last_update" class="one-third" style="float:right;font-size:18px;">
+				Last Updated:
+			</label>
+		</div>
+		<div class="input-container two-thirds" style="width:66%;float:right;">
+			<input type="text" name="last_update" disabled class="two-thirds" style="width:70%;float:left;" <?php if($this->shopAppOptions['last_update'] != null) { echo 'value="' . $this->shopAppOptions['last_update'] . '"';} ?>/>
+		</div>
+		<div class="clear"></div>
+		<br/>
+		<div class="label-container one-fifth" style="width:30%;float:left;">
+			<input type="submit" form="shopAppAdmin" class="button" id="force-update" style="float:right;" value="Manual Update"/>
+		</div>
+		</div>
+
+		<style>
+			.postbox-container {
+				float:none !important;
+			}
+		</style>
+
+
+		<?php
+		// dump($this->shopAppOptions);
+	}
+}
+
+/**
+ * dump function for debug
+ */
+if (!function_exists('dump')) {
+    function dump ($var, $label = 'Dump', $echo = TRUE) {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+        $output = '<pre style="background: #FFFEEF; color: #000; border: 1px dotted #000; padding: 10px; margin: 10px 0; text-align: left; width: 100% !important; font-size: 12px !important;">' . $label . ' => ' . $output . '</pre>';
+        if ($echo == TRUE) {
+            echo $output;}else {return $output;}
+    }
+}
+
+$shopperApprovedCache = new RRShopApp();
+
+?>
---
+++ lib/rrShopApp/rich-reviews-ShopAppOptions.php
@@ -0,0 +1,205 @@
+<?php
+
+
+
+class RRShopAppOptions {
+
+	var $defaults;
+
+	var $options_name;
+
+	 var $updated = FALSE;
+
+	function __construct($parent) {
+
+		$this->parent = $parent;
+		if (isset($_POST['dinner'])) {
+            $this->updated = $_POST['dinner'];
+        }
+		$this->options_name = $this->parent->option_name;
+		$this->defaults = array(
+			'api_url' 		=> 	'',
+			'markup'		=>	'',
+			'last_update'	=> 	'',
+      'site_id' => '',
+      'site_token' => '',
+      'reviews_last_pulled' => 'not yet pulled',
+      'total_review_count' => NULL,
+      'reviews_pulled_count' => NULL,
+      'average_score' => NULL,
+      'imported_review_ids' => array()
+		);
+
+		if ($this->get_option() == FALSE) {
+            $this->set_to_defaults();
+        }
+	}
+
+	public function set_to_defaults() {
+        delete_option($this->options_name);
+        foreach ($this->defaults as $key=>$value) {
+            $this->update_option($key, $value);
+        }
+    }
+
+	public function update_options() {
+		// $this->set_to_defaults();
+		if(isset($_POST["dinner"]) && $_POST['dinner'] == "served") {
+        dump($_POST['api_url']);
+	      $current_settings = $this->get_option();
+        $clean_current_settings = array();
+        foreach ($current_settings as $k=>$val) {
+            if ($k != NULL) {
+                $clean_current_settings[$k] = $val;
+            }
+        }
+
+        $this->defaults = array_merge($this->defaults, $clean_current_settings);
+        $update = array_merge($this->defaults, $_POST);
+        if(isset($update['api_url']) && ($update['api_url'] != '') && ($update['api_url'] != NULL)) {
+            $update = $this->parent->process_cache_update($update);
+            if(isset($update) && $update != NULL){
+                $data = array();
+                foreach ($update as $key=>$value) {
+                    if ($key != 'dinner' && $key != NULL) {
+                        $data[$key] = $value;
+                    }
+                }
+                // $data = $this->parent->update_site_keys($data);
+                // $data = $this->update_reviews_info($data);
+
+                $this->update_option($data);
+                $_POST['dinner'] = NULL;
+                $this->updated = 'wpm-update-options';
+                $this->parent->shopAppOptions = $this->get_option();
+            }
+            // dump('error contacting api/processing cache update');
+        } else {
+          //Maybe output messages...
+          // dump('api_url not set properly');
+        }
+        return;
+    }
+
+    if(isset($_POST['Whoop']) && $_POST['Whoop'] == 'There it is') {
+        $this->parent->process_reviews_pull();
+    }
+}
+
+
+
+	// From metabox v1.0.6
+
+  /**
+  * Gets an option for an array'd wp_options,
+  * accounting for if the wp_option itself does not exist,
+  * or if the option within the option
+  * (cue Inception's 'BWAAAAAAAH' here) exists.
+  * @since  Version 1.0.0
+  * @param  string $opt_name
+  * @return mixed (or FALSE on fail)
+  */
+  public function get_option($opt_name = '') {
+     $options = get_option($this->options_name);
+
+     // maybe return the whole options array?
+     if ($opt_name == '') {
+         return $options;
+     }
+
+     // are the options already set at all?
+     if ($options == FALSE) {
+         return $options;
+     }
+
+     // the options are set, let's see if the specific one exists
+     if (! isset($options[$opt_name])) {
+         return FALSE;
+     }
+
+     // the options are set, that specific option exists. return it
+     return $options[$opt_name];
+  }
+
+  /**
+  * Wrapper to update wp_options. allows for function overriding
+  * (using an array instead of 'key, value') and allows for
+  * multiple options to be stored in one name option array without
+  * overriding previous options.
+  * @since  Version 1.0.0
+  * @param  string $opt_name
+  * @param  mixed $opt_val
+  */
+  public function update_option($opt_name, $opt_val = '') {
+     // ----- allow a function override where we just use a key/val array
+     if (is_array($opt_name) && $opt_val == '') {
+         foreach ($opt_name as $real_opt_name => $real_opt_value) {
+             $this->update_option($real_opt_name, $real_opt_value);
+         }
+     }
+     else {
+         $current_options = $this->get_option(); // get all the stored options
+
+         // ----- make sure we at least start with blank options
+         if ($current_options == FALSE) {
+             $current_options = array();
+         }
+
+         // ----- now save using the wordpress function
+         $new_option = array($opt_name => $opt_val);
+         update_option($this->options_name, array_merge($current_options, $new_option));
+     }
+  }
+
+  /**
+  * Given an option that is an array, either update or add
+  * a value (or data) to that option and save it
+  * @since  Version 1.0.0
+  * @param  string $opt_name
+  * @param  mixed $key_or_val
+  * @param  mixed $value
+  */
+  public function append_to_option($opt_name, $key_or_val, $value = NULL, $merge_values = TRUE) {
+     $key = '';
+     $val = '';
+     $results = $this->get_option($opt_name);
+
+     // ----- always use at least an empty array!
+     if (! $results) {
+         $results = array();
+     }
+
+     // ----- allow function override, to use automatic array indexing
+     if ($value === NULL) {
+         $val = $key_or_val;
+
+         // if value is not in array, then add it.
+         if (! in_array($val, $results)) {
+             $results[] = $val;
+         }
+     }
+     else {
+         $key = $key_or_val;
+         $val = $value;
+
+         // ----- should we append the array value to an existing array?
+         if ($merge_values && isset($results[$key]) && is_array($results[$key]) && is_array($val)) {
+                 $results[$key] = array_merge($results[$key], $val);
+         }
+         else {
+                 // ----- don't care if key'd value exists. we override it anyway
+                 $results[$key] = $val;
+         }
+     }
+
+     // use our internal function to update the option data!
+     $this->update_option($opt_name, $results);
+  }
+
+  public function update_messages() {
+    if ($this->updated == 'served') {
+        echo '<div class="updated">The options have been successfully updated.</div>';
+        $this->updated = FALSE;
+    }
+	}
+}
