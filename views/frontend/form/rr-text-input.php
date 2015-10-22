d90fbdc6a3c13d171888115334e16202367c9f06
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-10-22 15:10:09 -0700

    disable field for logged in user

--- views/frontend/form/rr-text-input.php
+++ views/frontend/form/rr-text-input.php
@@ -7,6 +7,6 @@
 		</td>
 		<td class="rr_form_input">
 			<?php echo '<span class="form-err">' . $error . '</span>'; ?>
-			<input class="rr_small_input" type="text" name="r<?php echo $inputId; ?>" value="<?php echo $rFieldValue ; ?>" />
+			<input class="rr_small_input" type="text" name="r<?php echo $inputId; ?>" value="<?php echo $rFieldValue ; ?>" <?php if($disable) { echo 'disabled'; } ?> />
 		</td>
 	</tr>
