2123768184e29e764dc22e098e8d563c4553b9de
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-10-26 20:41:55 -0700

    placeholder markuo for image upload form field

---
+++ views/frontend/form/rr-img-input.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+	<tr class="rr_form_row">
+		<td class="rr_form_heading <?php if($require){ echo 'rr_required'; } ?>" >
+			<?php echo $label; ?>
+		</td>
+		<td>
+			<input type="text" placeholder="This is gonna be changed into an image input field" value="" />
+		</td>
+	</tr>
