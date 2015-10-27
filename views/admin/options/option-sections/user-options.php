2df944d8594855a6470c5fe78e9b8a0efdeb3cb8
Charlie Maxwell <charliemaxwell@nuancedmedia.com>
2015-10-26 20:41:01 -0700

    adding markup and conditional javacript support for unregistered user image upload

--- views/admin/options/option-sections/user-options.php
+++ views/admin/options/option-sections/user-options.php
@@ -6,6 +6,18 @@
 </label>
 <br />
 <div id="userOptionsMain" style="padding:8px;">
+<input type="checkbox" name="require-login" value="checked" id="loginGate"<?php echo $options['require-login'] ?> />
+	<label for="require-login">
+		<?php _e('Require Users to be logged in to submit reviews.', 'rich-reviews'); ?>
+	</label>
+	<div id="loginRequiredSub" style="padding: 13px;">
+		<label for="login-url">
+			<?php _e('Login Url for link in Review Gate', 'rich-reviews'); ?>
+		</label>
+		<br />
+		<input type="text" name="login-url" value="<?php if(isset($options['login-url']) && $options['login-url'] != '') { echo $options['login-url']; } else { echo wp_login_url(); } ?>" style="width: 90%;"/>
+	</div>
+	<br />
 	<input type="checkbox" name="form-name-use-usernames" value="checked" <?php echo $options['form-name-use-usernames'] ?> />
 	<label for="form-name-use-usernames">
 		<?php _e('Autofill name from User Accounts.', 'rich-reviews'); ?>
@@ -16,15 +28,20 @@
 		<?php _e('Use user avatars.', 'rich-reviews'); ?>
 	</label>
 	<br />
-	<input type="checkbox" name="require-login" value="checked" id="loginGate"<?php echo $options['require-login'] ?> />
-	<label for="require-login">
-		<?php _e('Require Users to be logged in to submit reviews.', 'rich-reviews'); ?>
-	</label>
+
 	<div id="userOptionsUnregisteredSub">
-		<input type="checkbox" name="unregistered-allow-avatar-upload" value="checked" <?php echo $options['unregistered-allow-avatar-upload'] ?> />
+		<input type="checkbox" name="unregistered-allow-avatar-upload" id="unregisteredUpload" value="checked" <?php echo $options['unregistered-allow-avatar-upload'] ?> />
 		<label for="unregistered-allow-avatar-upload">
 			<?php _e('Allow image upload for non-logged in reviewers, to be used in place of user avatar in review display.', 'rich-reviews'); ?>
 		</label>
+		<div id="unregisteredAvatarAllowedSub" style="padding:13px;">
+			<h4><?php _e('Reviewer Image Field', 'rich-reviews'); ?></h4>
+			<label for="form-reviewer-image-label">Form Label:</label>
+			<input type="text" name="form-reviewer-image-label" value="<?php echo $options['form-reviewer-image-label']; ?>" />
+			<br />
+			<label for="form-reviewer-image-require">Require Field: </label>
+			<input type="checkbox" name="form-reviewer-image-require" value="checked" <?php echo $options['form-reviewer-image-require'] ?> />
+		</div>
 	</div>
 	<br />
 </div>
@@ -34,11 +51,17 @@
 	jQuery(function() {
 		checkParentCondition('input[id="userOptionToggle"]:checked', '#userOptionsMain');
 		checkParentCondition('input[id="loginGate"]:checked', '#userOptionsUnregisteredSub', true);
+		checkParentCondition('input[id="loginGate"]:checked', '#loginRequiredSub');
+		checkParentCondition('input[id="unregisteredUpload"]:checked', '#unregisteredAvatarAllowedSub');
 		jQuery('#userOptionToggle').click(function() {
 			checkParentCondition('input[id="userOptionToggle"]:checked', '#userOptionsMain');
 		});
 		jQuery('#loginGate').click(function(){
+			checkParentCondition('input[id="loginGate"]:checked', '#loginRequiredSub');
 			checkParentCondition('input[id="loginGate"]:checked', '#userOptionsUnregisteredSub', true);
+		});
+		jQuery('#unregisteredUpload').click(function(){
+			checkParentCondition('input[id="unregisteredUpload"]:checked', '#unregisteredAvatarAllowedSub');
 		});
 	});
 
@@ -63,9 +86,3 @@
 		}
 	}
 </script>
-
-<?php dump($options['integrate-user-info']); ?>
-<?php dump($options['form-name-use-usernames']); ?>
-<?php dump($options['form-name-use-avatar']); ?>
-<?php dump($options['require-login']); ?>
-<?php dump($options['unregistered-allow-avatar-upload']); ?>
