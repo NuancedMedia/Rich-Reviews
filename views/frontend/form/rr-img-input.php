3f3a1c797aca9923a2065137086ac3e52a7f641d
charliemaxwell@nuancedmedia.com <charliemaxwell@nuancedmedia.com>
2016-02-24 11:00:39 -0700

    errors in the form need a state to identify for scroll

--- views/frontend/form/rr-img-input.php
+++ views/frontend/form/rr-img-input.php
@@ -6,7 +6,7 @@
 			<?php echo $label; ?>
 		</td>
 		<td class="rr_form_input">
-			<?php echo '<span class="form-err">' . $error . '</span>'; ?>
+			<span class="form-err<?php if ($error != '') { echo ' shown'; } ?>"><?php echo $error; ?></span>
 			<input type="text" name="rrInsertReviewerImageDisplay" id="rrInsertReviewerImageDisplay" class="rr_small_input" readonly/>
 			<input type="file" name="rrInsertReviewerImageFile" size="1" id="rrInsertReviewerImageFile" style="visibility:hidden;height:0px;" />
 			<button type="button" class="front_upload_image_button" style="margin-top:0;font-size:70%;padding:3px 5px;">Upload/Update Image</button>
@@ -14,25 +14,25 @@
 
 
 		<script type="text/javascript">
-			jQuery('.front_upload_image_button').live('click', function( event ){
-				console.log("hahahahah");
-				jQuery('#rrInsertReviewerImageFile').click();
-			});
-			jQuery('#rrInsertReviewerImageFile').change(function() {
-				files = jQuery('#rrInsertReviewerImageFile').get(0).files;
-				filesString = '';
-				numFiles = files.length;
-				count = 0;
-				jQuery.each(files, function() {
-					count++;
-					if(count == numFiles) {
-						filesString += this.name;
-					} else {
-						filesString += this.name + ', ';
-					}
-
+			jQuery(function() {
+				jQuery('#<?php echo $options['unique_key']; ?>').find('.front_upload_image_button').click(function( event ){
+					jQuery('#<?php echo $options['unique_key']; ?>').find('#rrInsertReviewerImageFile').click();
 				});
-				jQuery('#rrInsertReviewerImageDisplay').val(filesString);
+				jQuery('#<?php echo $options['unique_key']; ?>').find('#rrInsertReviewerImageFile').change(function(event) {
+					files = jQuery('#rrInsertReviewerImageFile').get(0).files;
+					filesString = '';
+					numFiles = files.length;
+					count = 0;
+					jQuery.each(files, function() {
+						count++;
+						if(count == numFiles) {
+							filesString += this.name;
+						} else {
+							filesString += this.name + ', ';
+						}
+					});
+					jQuery('#<?php echo $options['unique_key']; ?>').find('#rrInsertReviewerImageDisplay').val(filesString);
+				});
 			});
 		</script>
 	</tr>
